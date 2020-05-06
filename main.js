a68e720bf9b8e23ce87a57ae193c7e8e48ae8ae6
scarter13 <scarter13@gmail.com>
2020-05-06 18:38:42 -0400

    grabbed a name and logged it

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 
-function Test (){
-    let testing = "Testing, 1, 2, testing"
-    console.log (testing)
-}
+// function Test (){
+//     let testing = "Testing, 1, 2, testing"
+//     console.log (testing)
+// }
 
-Test ()
+// Test ()
+
+// function createHeaderElement (headline) {
+//     let headerEl = document.createElement ("h1")
+//     let headerEl.innerText = `${headline}`
+//     return headerEl
+// }
 
 
 fetch ('https://api.github.com/users/scarter13')
 .then (function (response) {
-    console.log (response)
+    // console.log (response)
+    return response.json ()
 })
+.then (function (data) {
+    console.log (data)
+    console.log (data.name)
+})
+// let targetBodyElement = document.querySelector ("body")
+// let headerElement = document.createElement ("h1")
+// headerElement.innerText = (data.name)
+// console.log (headerElement)
+// targetBodyElement.appendChild(headerElement)
+
+
+// let headerElement = document.querySelector("h1")
+
+
+// console.log (targetElement)
+// console.log (headerElement)
+// console.log (headerEl)
+
+
+
+
